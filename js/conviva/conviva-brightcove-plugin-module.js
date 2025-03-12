8fd5cd39d97a902e7b71b28140c16108049c136c
kamalsingh2024 <158989772+kamalsingh2024@users.noreply.github.com>
2025-03-12 14:08:39 +1100

    remove ads

--- js/conviva/conviva-brightcove-plugin-module.js
+++ js/conviva/conviva-brightcove-plugin-module.js
@@ -561,7 +561,7 @@ var convivaBcIntegration = function(n, i) {
             h.C("loadstart", n);
 
             buildMetadata(this, h.h);
-            this.ima3.settings.serverUrl = 'https://kamalsingh2024.github.io/adobelaunch/Pre-roll.xml?sz=640x480&iu=/7414/TEL.AFL/cx-on-stream&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]&ad_rule=1&cmsid=2513968&vid=[mediainfo.id]';
+            //this.ima3.settings.serverUrl = 'https://kamalsingh2024.github.io/adobelaunch/Pre-roll.xml?sz=640x480&iu=/7414/TEL.AFL/cx-on-stream&impl=s&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]&ad_rule=1&cmsid=2513968&vid=[mediainfo.id]';
 
         }),
         h.Tn("firstplay", function(n) {
