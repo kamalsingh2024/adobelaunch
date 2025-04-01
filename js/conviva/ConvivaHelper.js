19a0c6007fe7802d1e87934490a1bd6901134636
kamalsingh2024 <158989772+kamalsingh2024@users.noreply.github.com>
2025-04-01 16:24:43 +1100

    save

--- js/conviva/ConvivaHelper.js
+++ js/conviva/ConvivaHelper.js
@@ -411,12 +411,15 @@ var ConvivaIma3Integration = function (player, convivaConfiguration, contentMeta
         
     };
     this.onPlaying = function () { log("<<++onPlaying - " + player.mediainfo.name, prod);
+
+        if (!sessionActive) {
+            sessionActive = true; 
+            this.monitoringSessionInit();
+        } //make a new session if not active one already
+
         if (convivaVideoAnalytics != null) {
             convivaVideoAnalytics.reportPlaybackMetric(Conviva.Constants.Playback.PLAYER_STATE, Conviva.Constants.PlayerState.PLAYING);
         }
-        if (!sessionActive) {
-            sessionActive = true; this.monitoringSessionInit();
-        } //make a new session if not active one already
     };
     this.onPause = function () { log("<<++onPause - " + player.mediainfo.name, prod);
         if (convivaVideoAnalytics != null) {
